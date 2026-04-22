1b2081cdef31f5e0d8fada61ad5fc4d844d3133f
Digvijay <>
2026-04-22 16:15:59 +0530

    URL base change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/blissful/',
+  base: '/',
   publicDir: 'public',
   server: {
     port: 1234,
